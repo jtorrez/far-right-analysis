a299e5e9858181404ffe743aa93ef957ffa3a63a
Ronald K. Ning <ronald.ning@gmail.com>
2017-04-01 17:03:07 -0700

    clean-up

--- hate-speech-classifier/src/preprocessing.py
+++
@@ -1,6 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-def split_tweets_to_arrays(tweet_df):
-    X = tweet_df['tweet_text'].values
-    y = tweet_df['labels'].values
-    return X, y
